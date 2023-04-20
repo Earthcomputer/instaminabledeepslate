1967ce6f56e2d7aea54bc80d940c414655d613ff
Joe <burtonjae@hotmail.co.uk>
2023-04-20 23:12:33 +0100

    Update to 1.19.4

--- src/main/java/net/earthcomputer/instaminabledeepslate/ModTags.java
+++ src/main/java/net/earthcomputer/instaminabledeepslate/ModTags.java
@@ -1,10 +1,10 @@
 package net.earthcomputer.instaminabledeepslate;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
-    public static final TagKey<Block> NETHERITE_INSTAMINABLE = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("instaminabledeepslate", "netherite_instaminable"));
+    public static final TagKey<Block> NETHERITE_INSTAMINABLE = TagKey.create(Registries.BLOCK, new ResourceLocation("instaminabledeepslate", "netherite_instaminable"));
 }
